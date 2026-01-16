582af826de305e60851a6a64ec2665aa720f26fa
vxdbxn18 <vlad10112002@gmail.com>
2026-01-16 22:35:44 +0300

    fixed bugs

--- src/main/java/com/kata/springmvc/dao/UserDAOImpl.java
+++ src/main/java/com/kata/springmvc/dao/UserDAOImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class gUserDAOImpl implements UserDAO {
+public class UserDAOImpl implements UserDAO {
 
     @PersistenceContext
     private EntityManager entityManager;
